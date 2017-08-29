06a1e5eafbd4b80bf195d5b966c1fe4c108dd434
Gao Yisheng <1160139211@qq.com>
2017-08-29 21:16:52 +0800

    编译提交等功能合并,拆分提交

--- src/main/java/cn/edu/sdut/softlab/controller/HandInManager.java
+++ src/main/java/cn/edu/sdut/softlab/controller/HandInManager.java
@@ -124,68 +124,6 @@ public class HandInManager implements Serializable {
 
 
 	/**
-	 * *********************************************************************************
-	 * core 1/3 保存编辑内容 到数据库=<<achievement>>
-	 * 1.获取题目[PId]，学生学号[SId],获取文件命名[fileName]
-	 * 2.获取{editor}的值[value],保存到 表<achie>的 (answer)字段中
-	 * 3.把 "SId/PId/fileName" 作为路径，保存到 表<achie> 的(answerPath)中
-	 * 4.
-	 * *********************************************************************************
-	 */
-	public void saveFile() throws Exception {
-
-		//FilePath 以 /home/morpheus/ejosData/为基础， uid/qid为动态值．
-		File dirFile = new File(constitutePath());
-	   if(!dirFile.exists()){  //如果目录不存在
-			dirFile.mkdirs();
-		}
-		File sourceFile = new File( constitutePath() + expReport.getClassName() +".java");//保存源代码
-
-		try {
-
-		    if(sourceFile.exists()){
-		      sourceFile.delete();
-		     }
-
-		    FileWriter fr = new FileWriter(sourceFile);  //将文件保存起来
-		    BufferedWriter bw = new BufferedWriter(fr);
-
-		     //除标签
-		    String writeString = expReport.getAnswerText().replaceAll("<br>","").replaceAll("&nbsp;","");
-
-		    bw.write(writeString);//将获取的代码内容存到文件中
-		    bw.close();
-		    fr.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-
-		}//save done
-	}
-
-	/**
-	 * *********************************************************************************
-	 * core 2/3 保存并编译。保存到数据库=<<achievement>>，保存到文件
-	 * 1.获取题目[PId]，学生学号[SId],[StuNO],获取文件命名[fileName]
-	 * 2.获取{editor}的值[value],保存到表<achie> 的(answer)字段中
-	 * 3.把 "StuNO/PId/fileName" 作为路径，保存到 表<achie>的(answerPath)中
-	 * 4.开启线程，调用 #javac 、#java 命令,并获取返回值[return]
-	 * 5.将返回值[return]写入表<achie> 的(result)字段中
-	 * 6.比较[return]与 表<Question>中的(result)字段是否相等 => - 通过80 .eqaul()
-	 *                                                              - 未通过 20
-	 *                  存到表<achie>的(score)字段中
-	 * 7.
-	 * *********************************************************************************
-	 *
-	 * 0x01:把编译结果返回到页面，得到通过与否
-	 */
-	void saveAndCompile() {
-
-
-	}
-
-	/**
 	 * ********************************************************************************
 	 * core 3/3 保存\编译。并退出。 保存到数据库=<<achievement>>，保存到文件。返回至列表页
 	 * 1.获取题目[PId]，学生学号[SId],[StuNO],获取文件命名[fileName]
@@ -200,136 +138,6 @@ public class HandInManager implements Serializable {
 	 *
 	 * 0x01:不把编译结果返回到页面，直接跳转到OK页面
 	 */
-	public String saveCompileExit() {
-
-		return "../student/home.xhtml?faces-redirect=true";
-	}
-
-	/**
-	 * just compile:
-	 *
-	 */
-	public void compileJava(){
-
-		log.info("调用compileJava()");
-		Runtime runtime = Runtime.getRuntime();
-		try {
-			File sourceFile = new File(constitutePath() + expReport.getClassName() + ".class");//如果文件存在，则删除该文件
-			if (sourceFile.exists()) {
-				log.info("调用" + expReport.getClassName() + ".class--sourceFile.delete();");
-				sourceFile.delete();
-			}
-
-			File dir = new File(constitutePath());
-			File outputFile = new File(constitutePath()+"output.txt");//如果文件存在，则删除该文件
-			if (outputFile.exists()) {
-				log.info("调用output.txt--sourceFile.delete();");
-				outputFile.delete();
-			}
-
-
-			String cmdCompile = "javac " + expReport.getClassName() + ".java 2>> output.txt";
-
-			String[] cmdarray = {
-					"/bin/sh",
-					"-c",
-					cmdCompile
-					};
-			log.info("调用"+ cmdarray);
-			try {
-				runtime.exec(cmdarray,null,dir).waitFor();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-	}
-
-	public void runJava(){
-
-		log.info("调用runJava()");
-		Runtime runtime = Runtime.getRuntime();
-			try {
-
-				File dir = new File(constitutePath());
-
-				String cmd = "java " + expReport.getClassName() + " >> output.txt";
-
-				String[] cmdarray = {
-						"/bin/sh",
-						"-c",
-						cmd
-						};
-				log.info("调用"+ cmdarray.toString());
-				runtime.exec(cmdarray,null,dir).waitFor();
-
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-	}
-
-	public void showResult()throws Exception {
-		log.info("调用showResult()");
-		//读取文件，并返回到前台
-      String result = "";
-		try {
-
-			File resultFile = new File(constitutePath()+"output.txt");
-			if (!resultFile.exists()) {
-				//如果文件不存在，创建一个文件
-				resultFile.createNewFile();
-			}
-
-         InputStreamReader read = new InputStreamReader(new FileInputStream(resultFile));//考虑到编码格式
-         BufferedReader bufferedReader = new BufferedReader(read);
-         String line = null;
-         while((line = bufferedReader.readLine()) != null){
-			     result = result + "\n" + line;
-			     log.info(result);
-			}
-
-      read.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		expReport.setResult(result);//返回，并由button刷新到前台
-
-		try {
-			utx.begin();
-
-			Achievement a = achievementService.findByQuestionAndStudent(currentQuestion, currentUser);
-			//如果存在（查询到）该条目，则更新
-			if(!(a==null)){
-
-				a.setAnswer(expReport.getAnswerText());
-				a.setResult(result);
-				a.setScore(calculateScore(result,currentQuestion.getResult()));
-
-			   achievementService.edit(a);
-			//否则创建
-		   }else{
-			   Achievement ach=new Achievement(
-						expReport.getAnswerText(),
-						constitutePath(), result,
-						calculateScore(result,currentQuestion.getResult()),
-						currentQuestion, currentUser);
-			   achievementService.create(ach);
-		   	}
-		} finally {
-			utx.commit();
-		}
-
-		}
 
 	/**
 	 * 一键编译拿到结果
@@ -344,6 +152,7 @@ public class HandInManager implements Serializable {
 
 		   //读取文件内容，并返回到前台
 		   String result = "";
+	       String errresult = "";
 			File sourceFile = new File( constitutePath() + expReport.getClassName() +".java");//保存源代码
 
 			try {
@@ -357,8 +166,6 @@ public class HandInManager implements Serializable {
 
 			     //除标签
 			    String writeString = expReport.getAnswerText().replaceAll("<[^>]*>", "").replaceAll("&nbsp;","");
-			    							/*.replaceAll("&nbsp;","").replaceAll("<div *>","")
-			    							.replaceAll("<span *>","")*/
 
 			    bw.write(writeString);//将获取的代码内容存到文件中
 			    bw.close();
@@ -397,8 +204,8 @@ public class HandInManager implements Serializable {
 			         BufferedReader bufferedReader = new BufferedReader(readerr);
 			         String errline = null;
 			         while((errline = bufferedReader.readLine()) != null){
-						     result = result + "\n" + errline;
-						}
+			        	   errresult = errresult + "\n" + errline;
+					  }
 
 			         readerr.close();
 					} catch (FileNotFoundException e) {
@@ -458,38 +265,27 @@ public class HandInManager implements Serializable {
 					e.printStackTrace();
 				}
 
+				result = errresult + result;
 				expReport.setResult(result);//返回，并由button刷新到前台
-
-				try {
-						utx.begin();
-						Achievement a = achievementService.findByQuestionAndStudent(currentQuestion, currentUser);
-						//如果存在（查询到）该条目，则更新
-						if(!(a==null)){
-
-							a.setAnswer(expReport.getAnswerText());
-							a.setResult(result);
-							a.setScore(calculateScore(result,currentQuestion.getResult()));
-							a.setAnswerPath(constitutePath());
-							
-						   achievementService.edit(a);
-						//否则创建
-                   }else{
-						   Achievement ach=new Achievement(
-									expReport.getAnswerText(),
-									constitutePath(), result,
-									calculateScore(result,currentQuestion.getResult()),
-									currentQuestion, currentUser);
-						   achievementService.create(ach);
-						}
-						utx.commit();
-				} catch (Exception e) {
-						e.printStackTrace();
-				} finally {
-					
-				}
-
 	}
 
+	public void handIn() {
+		try {
+			utx.begin();
+     	    Achievement ach = new Achievement(
+	                  	expReport.getAnswerText(),
+							constitutePath(),
+							expReport.getResult(),
+							calculateScore(expReport.getResult(),currentQuestion.getResult()),
+							currentQuestion, 
+							currentUser);
+			achievementService.create(ach);
+			utx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
 	/**
 	 * @return the achie
 	 */
@@ -521,7 +317,6 @@ public class HandInManager implements Serializable {
 	}
 
 	public void selectedChanged(ValueChangeEvent event) {
-	   System.out.println("logPrint >> ---------------QuestionManager-selectedChanged-value-is:" + event.getNewValue().toString());
 	   facesContext.addMessage(null, new FacesMessage("当前问题是： " + event.getNewValue().toString()));
 	}
 
